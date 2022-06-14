ae4587b4f5d677654f78050a384922f50f29a953
neb <NebulaBC>
2022-06-14 00:44:46 -0400

    Add i3 parity for my SilverBlue install

--- .xinit.sh
+++ .xinit.sh
@@ -1,5 +1,4 @@
 export BROWSER="firefox"
 setxkbmap -option caps:escape
-feh --bg-scale ~/dox/wallpapers/wallpapers/0016.jpg
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+feh --bg-scale ~/dox/wallpapers/0016.jpg
 ~/go/bin/zara
